8162bb294460d12538d6840143fca5129c9a6a5c
Stephen Murphy <34074914+setevnz@users.noreply.github.com>
2024-08-28 15:07:01 +1200

    Update streamlit_app.py
    Fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,8 @@ st.write("The name on the smoothie will be: ", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')) 
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')) 
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
-
-import streamlit as st
 
 ingredients_list = st.multiselect(
     "Choose upto 5 ingredients",
